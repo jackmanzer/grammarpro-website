8659d059df207607cc5dcde82d01c956e16c8d3e
Jack Manzer <jackmanzer@Jacks-MBP.attlocal.net>
2020-10-09 15:25:00 -0500

    first commit

---
+++ the-grammar-pro.js
@@ -0,0 +1,61 @@
+// /* Pagination */ 
+
+var x = document.getElementById("page-display") 
+
+
+
+document.getElementById("hamburger-menu").onclick = function() { document.getElementById("body").style.cssText = "position: fixed"};
+
+
+
+document.getElementById("page1").onclick = function(){ x.src = "img/page1-large.png"};
+document.getElementById("page2").onclick = function(){ x.src = "img/page2-large.png"};
+document.getElementById("page3").onclick = function(){ x.src = "img/page3-large.png"};
+document.getElementById("page4").onclick = function(){ x.src = "img/page4-large.png"};
+document.getElementById("page5").onclick = function(){ x.src = "img/page5-large.png"};
+document.getElementById("page6").onclick = function(){ x.src = "img/page6-large.png"};
+document.getElementById("page7").onclick = function(){ x.src = "img/page7-large.png"};
+document.getElementById("page8").onclick = function(){ x.src = "img/page8-large.png"};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /*Test Javascript*/ 
+
+
+// /*Toggle Menu*/ 
+
+// var x = document.getElementById("hamburger-menu");
+// var y = document.getElementById("nav-toggle");
+// var z = document.getElementById("nav-li");
+
+// x.onclick=function() {
+//     var cur_style_val = y.style.transform.replace(/\s+/g, '')
+//     if (cur_style_val === "scale(1,0)") {
+//         y.style.transform = "scale(1,1)";
+//     } else { 
+//         y.style.transform = "scale(1,0)";   
+//     } 
+//     var cur_op_val = z.style.opacity.replace(/\s+/g, '') 
+//     if (cur_op_val === "0") {
+//         z.style.opacity = "1";
+//     } else { 
+//         z.style.opacity = "1";   
+//     }  
+// }
