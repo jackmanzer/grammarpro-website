bea5dced378a692c1e84f564c4e15cf2956eb4d2
Jack Manzer <jackmanzer@Jacks-MBP.attlocal.net>
2020-10-15 15:29:54 -0500

    Significantly debugging across the board, media query editing, improved organization

--- the-grammar-pro.js
+++ the-grammar-pro.js
@@ -1,21 +1,21 @@
 // /* Pagination */ 
 
-var x = document.getElementById("page-display") 
+// var x = document.getElementById("page-display") 
 
 
 
-document.getElementById("hamburger-menu").onclick = function() { document.getElementById("body").style.cssText = "position: fixed"};
+// document.getElementById("hamburger-menu").onclick = function() { document.getElementById("body").style.cssText = "position: fixed"};
 
 
 
-document.getElementById("page1").onclick = function(){ x.src = "img/page1-large.png"};
-document.getElementById("page2").onclick = function(){ x.src = "img/page2-large.png"};
-document.getElementById("page3").onclick = function(){ x.src = "img/page3-large.png"};
-document.getElementById("page4").onclick = function(){ x.src = "img/page4-large.png"};
-document.getElementById("page5").onclick = function(){ x.src = "img/page5-large.png"};
-document.getElementById("page6").onclick = function(){ x.src = "img/page6-large.png"};
-document.getElementById("page7").onclick = function(){ x.src = "img/page7-large.png"};
-document.getElementById("page8").onclick = function(){ x.src = "img/page8-large.png"};
+// document.getElementById("page1").onclick = function(){ x.src = "img/page1-large.png"};
+// document.getElementById("page2").onclick = function(){ x.src = "img/page2-large.png"};
+// document.getElementById("page3").onclick = function(){ x.src = "img/page3-large.png"};
+// document.getElementById("page4").onclick = function(){ x.src = "img/page4-large.png"};
+// document.getElementById("page5").onclick = function(){ x.src = "img/page5-large.png"};
+// document.getElementById("page6").onclick = function(){ x.src = "img/page6-large.png"};
+// document.getElementById("page7").onclick = function(){ x.src = "img/page7-large.png"};
+// document.getElementById("page8").onclick = function(){ x.src = "img/page8-large.png"};
 
 
 
